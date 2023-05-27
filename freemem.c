1269c7ff2957884f6ab3526996b1abbb34a396f2
OneSetCode <96799944+OneSetCode@users.noreply.github.com>
2023-05-27 11:49:48 -0700

    Update freemem.c

--- freemem.c
+++ freemem.c
@@ -1,20 +1,10 @@
 /* freemem.c
    implements freemem (free) for memory system
-   CSE 374 HW6
 */
 
 #include <assert.h>
 #include "mem.h"
 #include "mem_impl.h"
-
-/* This code will be filled in by the students.  Your primary job is to
-   implement freemem functions.  */
-
-/* initialize global variables? */
-
-/* Are there helper functions that only freemem needs?  Declare here. */
-
-/* Define your functions below: */
 
 void freemem(void* p) {
    check_heap();
